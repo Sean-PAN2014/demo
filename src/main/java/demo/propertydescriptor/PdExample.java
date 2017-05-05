8c06f856d3963b6740d3d7af6c1c77e3ecb3b803
Pan, Xinyang <seanpan2008@gmail.com>
2017-05-05 14:59:14 +0800

    PropertyDescriptor

---
+++ src/main/java/demo/propertydescriptor/PdExample.java
@@ -0,0 +1,15 @@
+package demo.propertydescriptor;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+import demo.util.vo.Person;
+
+public class PdExample {
+
+	public static void main(String[] args) throws IntrospectionException {
+		PropertyDescriptor pd = new PropertyDescriptor("name", Person.class);
+		System.out.println(pd);
+	}
+
+}
