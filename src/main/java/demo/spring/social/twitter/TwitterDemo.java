b98ac8256e901b00f1e4e6236294b7233068480f
Pan, Xinyang <seanpan2008@gmail.com>
2017-06-06 16:49:53 +0800

    polish twitter demo

--- src/main/java/demo/spring/social/twitter/TwitterDemo.java
+++ src/main/java/demo/spring/social/twitter/TwitterDemo.java
@@ -1,13 +1,10 @@
 package demo.spring.social.twitter;
 
-import java.util.List;
-
-import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 // http://www.baeldung.com/spring_social_twitter_setup
-public class TwitterDemoApplication {
+public class TwitterDemo {
 	private static final String consumerKey = "aWb8m15Uu9c8DTzm0DiCcDYXS";
 	private static final String consumerSecret = "YgWQgB8iusplAytmEuXVDdZ1rIxGCmRZXlCTq0nit0go3njWMo";
 	private static final String accessToken = "871934346285940736-GGovpoe8bt1kWL2FfcUncU5bK91Goe9";
@@ -16,10 +13,17 @@ public class TwitterDemoApplication {
 	public static void main(String[] args) {
 		// 
 		Twitter twitter = new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
-		List<Tweet> userTimeline = twitter.searchOperations().search("haha", 1).getTweets();
-		System.out.println(userTimeline);
-		System.out.println(userTimeline.get(0).getText());
-		System.out.println(twitter.timelineOperations().updateStatus("Hello World1!"));
+//		System.out.println(twitter.searchOperations().search("haha", 1).getTweets());
+		System.out.println(twitter.timelineOperations().updateStatus(message()));
+	}
+
+	private static String message() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("指標発表結果。13:30 [AUD] 豪準備銀行（中央銀行）、政策金利発表(前回結果:1.50％ 予想:1.50％ 結果:1.50％)").append(System.lineSeparator());
+		// http://min-fx.jp/market/indicators/
+		// http://bit.ly/c3xlcP
+		sb.append("http://bit.ly/c3xlcP  #fx #kawase");
+		return sb.toString();
 	}
 
 }
